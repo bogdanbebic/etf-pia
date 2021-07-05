d64b0f29175fe290a3abbb6ee837b3786b16942e
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-07-05 20:02:49 +0200

    backend/model: Add user model

---
+++ backend/src/model/user.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+let User = new Schema({
+    firstname: {
+        type: String
+    },
+    lastname: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    role: {
+        type: Number
+    }
+})
+
+export default mongoose.model('Users', User, 'users');
