7c9eb7107e634390255484b4d3d0a00de55abd17
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-07-05 20:05:07 +0200

    backend/server: Add login and allUsers route paths

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import userModel from './model/user';
 
 const app = express();
 
@@ -19,6 +20,29 @@ conn.once('open', () => {
 const router = express.Router();
 
 // TODO: routes
+router.route('/login').post((req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    userModel.findOne({'username': username, 'password': password}, 'username role', (err, user) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        res.json(user);
+    });
+});
+
+router.route('/allUsers').post((req, res) => {
+    userModel.find({}, (err, users) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        res.json(users);
+    });
+});
 
 app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
