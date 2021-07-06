310da26b700ba5d026d9227367237c48aaf9e9a3
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-07-06 13:17:28 +0200

    frontend/auth.guard: Add authorization service call to check auth

--- real-estate-agency/src/app/auth.guard.ts
+++ real-estate-agency/src/app/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthorizationService } from './authorization.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authorizationService.isAuthenticated(route.data.roles);
   }
 
+  constructor(private authorizationService: AuthorizationService) { }
 }
