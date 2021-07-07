d65b5af484fd4b66f94f29b91b28903a066038f7
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-07-07 16:55:28 +0200

    frontend/authorization.service: Refactor for user role subscriptions

--- real-estate-agency/src/app/authorization.service.ts
+++ real-estate-agency/src/app/authorization.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoggedInUser } from './logged-in-user';
 import { UserRoles } from './user-roles.enum';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,26 @@ export class AuthorizationService {
   uri = 'http://localhost:4000';
 
   currentUser: LoggedInUser;
+  currentUserRoleSubject = new Subject<UserRoles>();
 
   login(username: string, password: string) {
     const response = this.http.post(`${this.uri}/login`, { username: username, password: password });
-    response.subscribe((user: LoggedInUser) => this.currentUser = user);
-    return response;
+
+    const currentUserSubject = new Subject<LoggedInUser>();
+    response.subscribe(currentUserSubject);
+
+    currentUserSubject.subscribe((user: LoggedInUser) => {
+      this.currentUser = user;
+      if (user)
+        this.currentUserRoleSubject.next(user.role);
+    });
+
+    return currentUserSubject;
   }
 
   logout() {
     this.currentUser = null;
+    this.currentUserRoleSubject.next(UserRoles.Unregistered);
   }
 
   isAuthenticated(roles: UserRoles[]): boolean {
