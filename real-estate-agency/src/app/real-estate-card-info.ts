fdbfee9f90166c34c31a71db6b4976af36c0c765
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-07-08 14:05:20 +0200

    frontend: Add real-estate-card-info

---
+++ real-estate-agency/src/app/real-estate-card-info.ts
@@ -0,0 +1,9 @@
+export interface RealEstateCardInfo {
+  // TODO: picture
+  // TODO: address - municipality, city
+  price: number;
+  ishouse: boolean;
+  size: number;
+  numrooms: number;
+  renting: boolean;
+}
