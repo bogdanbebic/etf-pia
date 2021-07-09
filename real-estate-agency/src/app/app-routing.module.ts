30a39ee074c5e9b0a0940ef31726cb7a012583a3
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-07-09 14:24:44 +0200

    frontend/app-routing: Add routing and auth guard to top level components

--- real-estate-agency/src/app/app-routing.module.ts
+++ real-estate-agency/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { AuthGuard } from './auth.guard';
+import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { RealEstatesListComponent } from './real-estates-list/real-estates-list.component';
+import { RealEstatesMyListComponent } from './real-estates-my-list/real-estates-my-list.component';
+import { RealEstatesNewComponent } from './real-estates-new/real-estates-new.component';
+import { RealEstatesPromotedComponent } from './real-estates-promoted/real-estates-promoted.component';
+import { RealEstatesUnapprovedComponent } from './real-estates-unapproved/real-estates-unapproved.component';
 import { RegisterComponent } from './register/register.component';
+import { UserRoles } from './user-roles.enum';
+import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
   {
@@ -10,6 +21,51 @@ const routes: Routes = [
   },
   {
     path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'profile', component: AccountSettingsComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Registered, UserRoles.Agent, UserRoles.Admin] }
+  },
+  {
+    path: 'new-real-estate', component: RealEstatesNewComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Registered, UserRoles.Agent, UserRoles.Admin] }
+  },
+  {
+    path: 'my-real-estates', component: RealEstatesMyListComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Registered] }
+  },
+  {
+    path: 'unapproved-real-estates', component: RealEstatesUnapprovedComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Agent, UserRoles.Admin] }
+  },
+  {
+    path: 'promoted-real-estates', component: RealEstatesPromotedComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Agent] }
+  },
+  {
+    path: 'all-real-estates', component: RealEstatesListComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Agent] }
+  },
+  {
+    path: 'dashboard', component: DashboardComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Agent, UserRoles.Admin] }
+  },
+  {
+    path: 'admin-dashboard', component: DashboardAdminComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Admin] }
+  },
+  {
+    path: 'user-list', component: UsersListComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [UserRoles.Admin] }
   },
   {
     path: '', component: HomepageComponent
