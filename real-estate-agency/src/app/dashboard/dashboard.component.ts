6ea6d48140e3c4783696171eb15f9f6444757903
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-07-11 19:37:25 +0200

    frontend: Implement dashboard

--- real-estate-agency/src/app/dashboard/dashboard.component.ts
+++ real-estate-agency/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as c3 from "c3";
+import { RealEstateService } from '../real-estate.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private realEstateService: RealEstateService
+  ) { }
 
   ngOnInit(): void {
+    this.generateCharts();
+  }
+
+  generateCharts() {
+    this.generatePerCityChart();
+    this.generateHousesChart();
+    this.generateApartmentsChart();
+  }
+
+  generatePerCityChart() {
+    this.realEstateService.getDashboardPerCityData().subscribe((data: any[]) => {
+      let columns = [];
+      data.forEach(element => {
+        columns.push([element._id, element.total]);
+      });
+
+      c3.generate({
+        bindto: '#chart1',
+        interaction: {
+          enabled: false
+        },
+        data: {
+          type: 'bar',
+          columns: columns
+        }
+      });
+    });
+  }
+
+  generateHousesChart() {
+    this.realEstateService.getDashboardHousesRentSaleData().subscribe((data: any[]) => {
+      let columns = [];
+      data.forEach(element => {
+        columns.push([element._id ? 'For Rent' : 'For Sale', element.total]);
+      });
+
+      c3.generate({
+        bindto: '#chart2',
+        interaction: {
+          enabled: false
+        },
+        data: {
+          type: 'bar',
+          columns: columns
+        }
+      });
+    });
+  }
+
+  generateApartmentsChart() {
+    this.realEstateService.getDashboardApartmentsRentSaleData().subscribe((data: any[]) => {
+      let columns = [];
+      data.forEach(element => {
+        columns.push([element._id ? 'For Rent' : 'For Sale', element.total]);
+      });
+
+      c3.generate({
+        bindto: '#chart3',
+        interaction: {
+          enabled: false
+        },
+        data: {
+          type: 'bar',
+          columns: columns
+        }
+      });
+    });
   }
 
 }
